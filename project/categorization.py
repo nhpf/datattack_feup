b8c9ab40c84509c615f9b1c006077dfe481cb8d8
Rodrigo Gomes <rodrigocamara127@gmail.com>
2023-04-22 15:57:35 +0100

    First version of categorization after cleaning

--- project/categorization.py
+++ project/categorization.py
@@ -1,6 +1,10 @@
 # Imports
 import pandas as pd
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
 
 # Read the database .CSV
 ocorrencias = pd.read_csv(
@@ -18,37 +22,41 @@ ocorrencias['Longitude'] = pd.to_numeric(
 )
 
 # Define a function to extract "category2/category3" from a string
-def extract_category2_3(string):
-    parts = string.split("/", 1)
-    if len(parts) > 1:
-        return parts[1].rsplit("/", 1)[0]
+def extract_category3(string):
+    #print(string)
+    if string == 'nan' or any(str.isdigit(c) for c in string):
+        return
     else:
-        return ""
-
-#Verifying the uniqueness of occurence nature labeling
-
-categorias = ocorrencias['Natureza'].unique()
-
-print(categorias)
-
-
-# Compare length for unique elements
-if(len(set(categorias)) == len(categorias)):
-
-   print("All elements are unique.")
-else:
-   print("All elements are not unique.")
-
-print(len(categorias))
-
-for value in categorias:
-    ocorrencias['Natureza']
+        parts = string.split("/", 4)
+        return parts[2].strip()
     
+def extract_category2(string):
+    #print(string)
+    if string == 'nan' or any(str.isdigit(c) for c in string):
+        return
+    else:
+        parts = string.split("/", 4)
+        return parts[1].strip()
 
-ocorrencias['category2_3'] = ocorrencias['Natureza'].astype(str).apply(extract_category2_3)
 
-# Display the DataFrame with the new "category2_3" column
-print(ocorrencias['category2_3'])
-# Check the size and shape of the database
-#ocorrencias.shape
+#Reformulate categories based on functions defined
+ocorrencias['category2'] = ocorrencias['Natureza'].astype(str).apply(extract_category2)
+ocorrencias['category3'] = ocorrencias['Natureza'].astype(str).apply(extract_category3)
 
+#Print results of level 2 and level 3 nodes of category tree before consolidation
+#print(ocorrencias['category2'].value_counts())
+#print(ocorrencias['category3'].value_counts())
+
+#Consolidate categories with little statistical relevance
+relevance_threshold = 100
+category_counts = ocorrencias['category3'].value_counts()
+filtered_categories = category_counts[category_counts <= relevance_threshold].index.tolist()
+# Replace the filtered categories with a new "Other" category
+ocorrencias['category3'] = ocorrencias['category3'].replace(
+    to_replace=filtered_categories,
+    value='Outras ocorrências'
+)
+
+# Display the updated value counts
+print(ocorrencias['category2'].value_counts())
+print(ocorrencias['category3'].value_counts())
